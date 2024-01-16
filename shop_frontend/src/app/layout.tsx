2f9d82e9735ee573e8360465a3d439cbca820169
Mikhail Sergeev <90124193+red88july@users.noreply.github.com>
2024-01-16 20:57:48 +0600

    Add files via upload

---
+++ shop_frontend/src/app/layout.tsx
@@ -0,0 +1,35 @@
+'use client';
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
+import theme from '@/theme';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import AppToolbar from '@/components/UI/AppToolbar/AppToolbar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <CssBaseline/>
+          <html lang="en">
+          <body>
+          <header>
+            <AppToolbar/>
+          </header>
+          <main>
+            <Container maxWidth="xl">
+              {children}
+            </Container>
+          </main>
+          </body>
+          </html>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AppRouterCacheProvider>
+  );
+}
